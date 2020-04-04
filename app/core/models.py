cf8e5d110616bcc849398eac45338dafb6dd1eca
Aditya Prasad <adityaprasad246278@gmail.com>
2020-04-04 22:31:21 +0530

    added custom user module

--- app/core/models.py
+++ app/core/models.py
@@ -1,26 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,\
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
-        def create_user(self,email,password=None,**extra_fields):
-            """cretes and saves new user"""
-            if not email:
-                raise ValueError("user should have a email error")
-            user = self.model(email=self.normalize_email(email),**extra_fields)
-            user.set_password(password)
-            user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """cretes and saves new user"""
+        if not email:
+            raise ValueError("user should have a email error")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
-        def create_superuser(self,email,password):
-            """Creates a superuser and saves"""
-            user = self.create_user(email,password)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save(using=self._db)
+    def create_superuser(self, email, password):
+        """Creates a superuser and saves"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-            return user
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
